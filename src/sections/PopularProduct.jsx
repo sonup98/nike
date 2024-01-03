a452ff348fc4e6df1a820d2361817330df4e9631
sonu prajapat <sonuprajapat2398@gmail.com>
2024-01-03 18:33:42 +0530

    msg

---
+++ src/sections/PopularProduct.jsx
@@ -0,0 +1,11 @@
+
+
+const PopularProduct = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PopularProduct
---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,Footer,CustomerReviews,PopularProduct,Services,SpecialOffer,Subscribe,SuperQuality
+}
